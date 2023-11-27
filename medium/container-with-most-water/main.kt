01042a1666a2328e6616d449cf8be51c98ea2980
igordmv <igor_recreio23@hotmail.com>
2023-11-27 14:49:14 -0300

    container-with-most-water

---
+++ medium/container-with-most-water/main.kt
@@ -0,0 +1,32 @@
+//https://leetcode.com/problems/container-with-most-water
+fun maxArea(height: IntArray): Int {
+        var left = 0
+        var right = height.lastIndex
+        var maxWater = 0
+        var lowerBarUsed = 1
+        while (left < height.lastIndex) {
+            if (right > left) {
+                var lowerBar: Int
+                if (height[left] > height[right]) {
+                    lowerBar = height[right]
+                } else {
+                    lowerBar = height[left]
+                }
+                val area = lowerBar * (right-left)
+
+                if (area > maxWater) {
+                    maxWater = area
+                }
+                if (height[left] <= lowerBarUsed) {
+                    right = height.lastIndex
+                    left++
+                } else {
+                    right--
+                }
+            } else {
+                right = height.lastIndex
+                left++
+            }
+        }
+        return maxWater
+}
