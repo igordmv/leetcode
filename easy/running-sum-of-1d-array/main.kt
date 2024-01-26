859875d8b7f820058555e4f3946ceda4dbb92a39
igordmv <igor_recreio23@hotmail.com>
2024-01-25 22:39:23 -0300

    Running Sum of 1d Array

--- easy/running-sum-of-1d-array/main.kt
+++ easy/running-sum-of-1d-array/main.kt
@@ -1,4 +1,4 @@
-//https://leetcode.com/problems/reverse-string/
+//https://leetcode.com/problems/running-sum-of-1d-array/
 fun runningsSum(nums: IntArray): IntArray {
     for(i in 1 until nums.size){
         nums[i] = nums[i-1]+nums[i]
