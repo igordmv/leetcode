ff82281a1e3e87bc9829844e52fde15c6314e62d
igordmv <igor_recreio23@hotmail.com>
2024-07-05 21:22:28 -0300

    Majority Element

---
+++ easy/majority-element/main.kt
@@ -0,0 +1,21 @@
+//https://leetcode.com/problems/majority-element
+class Solution {
+    fun majorityElement(nums: IntArray): Int {
+        if (nums.size == 1) return nums[0]
+
+        val map = HashMap<Int, Boolean>()
+        var answer = 0
+        var majorityElement = 0
+        for (item in nums) {
+            if (!map.containsKey(item)) {
+                val count = nums.filter { it == item }.size
+                if (count > answer) {
+                    answer = count
+                    majorityElement = item
+                }
+                map[item] = true
+            }
+        }
+        return majorityElement
+    }
+}
