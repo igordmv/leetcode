90583101c7eec4fe8744209b47d6e98728d43c19
Igor Vieira <igor@Igors-MacBook-Pro-2.local>
2025-06-10 01:50:49 -0300

    Remove Duplicates from Sorted List

---
+++ easy/remove-duplicates-from-sorted-list/main.kt
@@ -0,0 +1,23 @@
+//https://leetcode.com/problems/remove-duplicates-from-sorted-list/
+/**
+ * Example:
+ * var li = ListNode(5)
+ * var v = li.`val`
+ * Definition for singly-linked list.
+ * class ListNode(var `val`: Int) {
+ *     var next: ListNode? = null
+ * }
+ */
+class Solution {
+    fun deleteDuplicates(head: ListNode?): ListNode? {
+        var h = head
+        while (h?.next != null) {
+            if (h?.`val` == h?.next?.`val`) {
+                h.next = h.next.next
+            } else {
+                h = h.next
+            }
+        }
+        return head
+    }
+}
