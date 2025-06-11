59977799cf9c69ef826ba39b685b64150784e724
Igor Vieira <igor@Igors-MacBook-Pro-2.local>
2025-06-11 01:18:07 -0300

    Remove All Adjacent Duplicates In String

--- easy/middle-of-the-linked-list/main.kt
+++ easy/middle-of-the-linked-list/main.kt
@@ -8,14 +8,12 @@
  *     var next: ListNode? = null
  * }
  */
-class Solution {
-    fun middleNode(head: ListNode?): ListNode? {
-        var fast = head
-        var slow = head
-        while (fast?.next != null) {
-            fast = fast.next.next
-            slow = slow?.next
-        }
-        return slow
+fun middleNode(head: ListNode?): ListNode? {
+    var fast = head
+    var slow = head
+    while (fast?.next != null) {
+        fast = fast.next.next
+        slow = slow?.next
     }
+    return slow
 }
