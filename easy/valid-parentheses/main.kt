613aba6f911eacda576f7cab6ee2562a9108bce3
igordmv <igor_recreio23@hotmail.com>
2025-07-02 03:26:01 -0300

    update valid parentheses

--- easy/valid-parentheses/main.kt
+++ easy/valid-parentheses/main.kt
@@ -1,16 +1,21 @@
 //https://leetcode.com/problems/valid-parentheses/
-fun isValid(s: String): Boolean {
-    val map = hashMapOf<Char, Char>( '(' to ')', '[' to ']', '{' to '}')
-    val mustCloseList = Stack<Char>()
-    s.forEach { char ->
-        if (char == '(' || char == '[' || char == '{') {
-            mustCloseList.push(char)
-        } else {
-            val closing = mustCloseList.pop()
-            if (map[closing] != char) {
+    fun isValid(s: String): Boolean {
+        if(s.length == 1) {
+            return false
+        }
+        val map = hashMapOf<Char, Char>( '(' to ')', '[' to ']', '{' to '}')
+        val mustCloseList = Stack<Char>()
+        s.forEach { char ->
+            if (char == '(' || char == '[' || char == '{') {
+                mustCloseList.push(char)
+            } else if (mustCloseList.size > 0){
+                val closing = mustCloseList.pop()
+                if (map[closing] != char) {
+                    return false
+                }
+            } else {
                 return false
             }
         }
+        return mustCloseList.size == 0
     }
-    return true
-}
